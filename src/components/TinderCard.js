553e309f96754f5926d9f32a44b0f5d9b564b860
Gabin Marignier <gabin.marignier@gmail.com>
2017-02-12 01:09:17 +0100

    Split code

--- src/components/TinderCard.js
+++ src/components/TinderCard.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import Youtube from 'react-native-youtube'
 
 class TinderCard extends Component {
 	render() {
 		return (
 			<View style={styles.card}>
 				<Text>Bite number {this.props.cards}</Text>
+				<YouTube
+					ref="youtubePlayer"
+					videoId="KVZ-P-ZI6W4" // The YouTube video ID
+					play={true}           // control playback of video with true/false
+					hidden={false}        // control visiblity of the entire view
+					playsInline={true}    // control whether the video should play inline
+					loop={false}          // control whether the video should loop when ended
+
+					onReady={(e)=>{this.setState({isReady: true})}}
+					onChangeState={(e)=>{this.setState({status: e.state})}}
+					onChangeQuality={(e)=>{this.setState({quality: e.quality})}}
+					onError={(e)=>{this.setState({error: e.error})}}
+					onProgress={(e)=>{this.setState({currentTime: e.currentTime, duration: e.duration})}}
+
+					style={{alignSelf: 'stretch', height: 300, backgroundColor: 'black', marginVertical: 10}}
+                />
 			</View>
 		)
 	}
