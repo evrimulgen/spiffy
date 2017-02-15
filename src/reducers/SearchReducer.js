3b028ea915a86468ad123147ddd272451bc5bba8
Gabin Marignier <gabin.marignier@gmail.com>
2017-02-15 19:48:18 +0100

    Improve search reducer

--- src/reducers/SearchReducer.js
+++ src/reducers/SearchReducer.js
@@ -7,12 +7,20 @@ import {
 
 const initialState = {
   keyword: '',
+  isFetching: false,
 }
 
 export function search(prevState = initialState, action) {
   switch (action.type) {
     case KEYWORD_CHANGED:
       return { ...prevState, keyword: action.payload }
+    case FETCH_VIDEOS_REQUEST:
+      return { ...prevState, isFetching: true }
+    case FETCH_VIDEOS_SUCCESS:
+      return { ...prevState, isFetching: false }
+    case FETCH_VIDEOS_FAILURE:
+      console.error(action.payload)
+      return { ...prevState, isFetching: false }
     default:
       return prevState
   }
