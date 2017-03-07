b1f51d6bcb418acd57f96d5d8a80e9646ff8cda5
Gabin Marignier <gabin.marignier@gmail.com>
2017-03-07 16:30:49 +0100

    Debug Youtube player by rendering when isReady true

--- src/components/Station/YoutubePlayer.js
+++ src/components/Station/YoutubePlayer.js
@@ -1,30 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import YouTube from 'react-native-youtube'
 import config from '../../config'
+import { WIDTH } from '../../styles'
 
 const propTypes = {}
 const defaultProps = {}
 
-function YoutubePlayer(props) {
-  return (
-    <YouTube
-      playsInline
-      play={props.play}
-      fs={false}
-      controls={1}
-      videoId={props.videoId}
-      onChangeState={props.onChangeState}
-      apiKey={config.YOUTUBE_KEY}
-      style={styles.youtube}
-    />
-  )
+class YoutubePlayer extends Component {
+  state = {
+    status: null,
+    isReady: false,
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <View style={styles.container}>
+        <YouTube
+          playsInline
+          play={this.props.play}
+          onReady={(e)=>{this.setState({isReady: true})}}
+          fs={false}
+          controls={2}
+          videoId={this.props.videoId}
+          onChangeState={(e) => {
+            this.setState({ status: e.state })
+            this.props.onChangeState(e)
+          }}
+          apiKey={config.YOUTUBE_KEY}
+          style={this.state.isReady ? styles.youtube : {}}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   youtube: {
-    height: 169,
-    width: 300,
+    height: 9 / 16 * WIDTH,
+    width: WIDTH,
   },
 })
 
